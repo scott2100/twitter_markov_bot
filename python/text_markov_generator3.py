dc1506a20aa53f4baf784406e2c3dc767d0fc405
scott young <syoung@scotts-MacBook-Pro.local>
2017-10-08 15:15:14 -0400

    Changed code to generate markov text from input text.  Need to test with larger corpus and improve output.

--- python/text_markov_generator3.py
+++ python/text_markov_generator3.py
@@ -1,39 +1,33 @@
 import collections
 from random import *
 
-inputString = "Presidents and their administrations have been talking to North Korea for 25 years, agreements made and massive amounts of money paid hasn't worked, agreements violated before the ink was dry, makings fools of U.S. negotiators. Sorry, but only one thing will work! "
+inputString = "Presidents and their administrations have been talking to North Korea for 25 years, agreements made and massive amounts of money paid hasn't worked, agreements violated before the ink was dry, makings fools of U.S. negotiators. Sorry, but only one thing will work! Stock Market hits an ALL-TIME high! Unemployment lowest in 16 years! Business and manufacturing enthusiasm at highest level in decades! Wow, so many Fake News stories today. No matter what I do or say, they will not write or speak truth. The Fake News Media is out of control! It is a miracle how fast the Las Vegas Metropolitan Police were able to find the demented shooter and stop him from even more killing! Wow, so many Fake News stories today. No matter what I do or say, they will not write or speak truth. The Fake News Media is out of control! I call my own shots, largely based on an accumulation of data, and everyone knows it. Some FAKE NEWS media, in order to marginalize, lies!"
 
 def findNgrams(inputString):
     ngrams = list()
-    for word in inputString.split()):
-        print(word)
-        #need code to get word pairs
+    inputString = inputString.split()
+    for i in range(len(inputString)-1):
+        ngram = inputString[i], inputString[i+1]
+        ngrams.append(ngram)
     return ngrams
 
-def countNgrams(ngramsList):
-    countedNgramsList = ''
-    countedNgrams = collections.Counter(ngramsList)
-    for key, value in countedNgrams.most_common():
-        countedNgramsList += key + ' : ' + str(value) + '\n'
-    return countedNgramsList
-
-def generateCorpus(splitString):
+def generateCorpus(inputString):
     ngramsDict = dict()
     ngram = tuple()
+
+    inputString = inputString.split()
+    for i in range(len(inputString)-1):
+        ngram = inputString[i], inputString[i+1]
     
-    for i in range(len(splitString)-1):
-        if (i < len(splitString)):
-            ngram = (splitString[i], splitString[i+1])
-            
-            #add ngram to dictionary with no codon key
-            if ngram not in ngramsDict:
-                ngramsDict[ngram] = []
+        #add ngram to dictionary with no codon key
+        if ngram not in ngramsDict:
+            ngramsDict[ngram] = []
             
             #loop through all ngrams, find the next codon
             #then add append codons to ngram values
-            if (i < len(splitString) - 2):
-                nextPossibleCodon = splitString[i+2]
-                nextPossibleCodonList = [splitString[i+2]]
+            if (i < len(inputString) - 2):
+                nextPossibleCodon = inputString[i+2]
+                nextPossibleCodonList = [inputString[i+2]]
                 if nextPossibleCodon:
                     if ngram in ngramsDict:
                         ngramsDict[ngram].append(nextPossibleCodon)
@@ -55,8 +49,10 @@ def generateText(seed):
     seedlist = seed.split()
     nextkey = seedlist[-2], seedlist[-1]
     while (len(fullText)<100):
+        print(len(nextkey))
         print("NEXTKEY: " + str(nextkey))
-        nextvalue = choice(corpus[nextkey])
+        if(len(nextkey)>0):
+             nextvalue = choice(corpus[nextkey])
         print("NEXTVALUE: " + nextvalue)
         keyandvalue = nextkey[-2] + " " + nextkey[-1] + " " + nextvalue
         print("keyandvalue: " + keyandvalue)
@@ -67,12 +63,8 @@ def generateText(seed):
         print("FULL TEXT: " + fullText)
 
 ngramsList = findNgrams(inputString)
-print(ngramsList)
-###countedNgrams = countNgrams(ngramsList)
-#print countedNgrams
-#corpus = generateCorpus(splitString)
-#print corpus
-#generateMarkovText(corpus)
-#generateSeed(corpus)
+corpus = generateCorpus(inputString)
+print(corpus)
+generateSeed(corpus)
 
 
