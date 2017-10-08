9f0fb679f99fc29bffba0fa3e8f941441968bf5e
scott young <syoung@scotts-MacBook-Pro.local>
2017-10-08 18:32:33 -0400

    Completed code to generate markov text words from corpus.  Need to test with larger corpus.

--- python/text_markov_generator3.py
+++ python/text_markov_generator3.py
@@ -48,23 +48,33 @@ def generateText(seed):
     print("SEED: " + seed)
     seedlist = seed.split()
     nextkey = seedlist[-2], seedlist[-1]
+    
+    nextvalue = choice(corpus[nextkey])
+    keyandvalue = nextkey[-2] + " " + nextkey[-1] + " " + nextvalue
+    fullText = fullText + " " + keyandvalue
+    
     while (len(fullText)<100):
-        print(len(nextkey))
-        print("NEXTKEY: " + str(nextkey))
-        if(len(nextkey)>0):
-             nextvalue = choice(corpus[nextkey])
-        print("NEXTVALUE: " + nextvalue)
-        keyandvalue = nextkey[-2] + " " + nextkey[-1] + " " + nextvalue
-        print("keyandvalue: " + keyandvalue)
-        fullText = fullText + " " + keyandvalue
         fullTextSplit = fullText.split()
         nextkey = fullTextSplit[-2], fullTextSplit[-1]
+        print(len(nextkey))
+        print("NEXTKEY: " + str(nextkey))
+        #skip adding key if it's empty
+        if(len(nextkey)>0):
+         nextvalue = choice(corpus[nextkey])
+        print("NEXTVALUE: " + nextvalue)
+        print("keyandvalue: " + keyandvalue)
+        print("fulltext split length: ",  len(fullText.split()))
+        if(len(fullText.split())>=3):
+           print("In Loop")
+           fullText = fullText + " " + nextvalue
+        print("FullTextSplit: " + str(fullTextSplit))
+        print("Next key 2: " + str(nextkey))
         print("TEXT: " + seed)
         print("FULL TEXT: " + fullText)
 
 ngramsList = findNgrams(inputString)
 corpus = generateCorpus(inputString)
-print(corpus)
+#print("Corpus: " + str(corpus))
 generateSeed(corpus)
 
 
